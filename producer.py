ec34d4e49a04a2986b1a2f660da58671179e0322
vivupadi <vivupadi@gmail.com>
2025-09-19 23:53:30 +0200

    class for Live data working

--- producer.py
+++ producer.py
@@ -50,24 +50,51 @@ class Live_OpenF1:
         self.access_token = os.getenv("password")
         self.mqtt_broker = "mqtt.openf1.org"
         self.mqtt_port = 8883
+
+    def setup(self)
+        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        client.username_pw_set(username=self.mqtt_username, password=self.access_token)
+        client.tls_set(cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLS_CLIENT)
+
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        try:
+            client.connect(mqtt_broker, mqtt_port, 60)
+            client.loop_forever() # Starts a blocking network loop
+        except Exception as e:
+            print(f"Connection error: {e}")
+
+    def on_connect(self, client, userdata, flags, rc, properties=None):
+        if rc == 0:
+            print("Connected to OpenF1 MQTT broker")
+            client.subscribe("v1/location")
+            client.subscribe("v1/laps")
+            # client.subscribe("#") # Subscribe to all topics
+        else:
+            print(f"Failed to connect, return code {rc}")
+
+    def on_message(self, client, userdata, msg):
+        print(f"Received message on topic '{msg.topic}': {msg.payload.decode()}")
+        data = json.loads(msg.payload.decode())
+
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.username_pw_set(username=mqtt_username, password=access_token)
+    client.tls_set(cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLS_CLIENT)
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    try:
+        client.connect(mqtt_broker, mqtt_port, 60)
+        client.loop_forever() # Starts a blocking network loop
+    except Exception as e:
+        print(f"Connection error: {e}")
+
     
 
     def explore_driver():
     
     def explore_laps():
     
-
-class Hist_OpenF1:(uses REST API)
-    def __init__(self, password, api_key):
-        self.mqtt_username = os.getenv("username")
-        self.access_token = os.getenv("password")
-        self.mqtt_broker = "mqtt.openf1.org"
-        self.mqtt_port = 8883
-    
-
-    def explore_driver():
-    
-    def explore_laps():
-    
-
         """
